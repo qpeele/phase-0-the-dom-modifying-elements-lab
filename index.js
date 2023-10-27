93ebf875015e83d3dba5cb441cb36658a2be5505
Quachell Peele <quachellpeele@gmail.com>
2023-10-26 22:44:34 -0400

    ughhh

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const mainElement = document.querySelector("main#main");
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Quachell is the champion";
+
+// Append the newHeader to the DOM
+document.body.appendChild(newHeader);
